3c3e09bb84e1e31c05a6c054fe6863d258b4ddc1
Xiabher <99346955+Xiabher@users.noreply.github.com>
2024-09-15 23:16:54 +0200

    updated the priority of list items

--- src/app/store/memoStore.ts
+++ src/app/store/memoStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { persist, createJSONStorage } from 'zustand/middleware'
 import { Memo } from '@/types';
 
 
@@ -34,7 +34,7 @@ const useMemoStore = create<MemoStore>()(
     }),
     {
       name: 'memo-storage',
-      getStorage: () => localStorage,
+      storage: createJSONStorage(() => localStorage),
     }
   )
 )
