0fa8bd16fde3044559b9d967a4cbc512ec55e7af
Xiabher <99346955+Xiabher@users.noreply.github.com>
2024-09-18 11:59:44 +0200

    installed next-pwa to make it a real pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  ...nextConfig
+});
