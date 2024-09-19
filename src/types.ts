4aaef6df372ede04ea5d7f7abff3336fa76c4bd8
Xiabher <99346955+Xiabher@users.noreply.github.com>
2024-09-19 11:55:17 +0200

    fixed category not exist in memos error

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,10 @@
-export interface Memo {
+
+  export interface Memo {
     id: number;
     text: string;
     completed: boolean;
+    createdAt: string;
+    updatedAt: string;
+    category?: string; // Add this line
+    tags?: string[];// I added this line just in case
   }
