80160a2dd38ee671425e7eb1b1d8cdf0ce940e7e
Xiabher <99346955+Xiabher@users.noreply.github.com>
2024-09-29 17:12:39 +0200

    fixing pwa error

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
-import withPWA from "next-pwa";
+import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // This is correct here
+  // Your other Next.js config options can go here
 };
 
 const pwaConfig = {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development", // This will disable PWA in development mode
-    // You can add any other options here if needed
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 };
 
-export default withPWA({
-    ...nextConfig,
-    ...pwaConfig,
-});
+export default withPWA(pwaConfig)(nextConfig);
